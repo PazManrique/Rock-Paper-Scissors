1cb4c2181fc48382a7e531ba96f9873ea4ce0bab
pilpod <giacomo.corso@gmail.com>
2022-08-18 08:59:32 +0200

    init repo

---
+++ tests/GameTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase {
+    
+    /** @test */
+    public function test_example()
+    {
+        $this->assertEquals(2,2);
+    }
+    
+}
